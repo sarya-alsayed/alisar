6058615ab4e7003b29941ae242c19c96cfd6465d
sarya-alsayed <test@tests-MacBook-Pro.local>
2020-12-19 13:06:17 +0200

    filter restaurants by cuisines

--- src/App.js
+++ src/App.js
@@ -10,15 +10,59 @@ class App extends Component {
       error: null,
       isLoaded: false,
       restaurants: [],
-      restaurantInfo:null,
+      restaurantInfo: null,
+      filterAttributes : {}
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleRestaurantClick(restaurant){
+  handleRestaurantClick(restaurant) {
     this.setState({
       restaurantInfo: restaurant
     });
+  }
 
+  handleInputChange(event) {
+    if(event.target.checked){
+      this.state.filterAttributes[event.target.id] = event.target.value; 
+    }else{
+      delete this.state.filterAttributes[event.target.id];
+    }    
+  }
+
+  filterRestaurantsByCuisines(e) {
+    e.preventDefault();
+    console.log(this.state.filterAttributes)
+    const attributes = this.state.filterAttributes;
+    var query = "";
+    for (const key in attributes){
+      query = key + '%2C' + query;
+    }
+    query = query.substring(0, query.length-3)
+    console.log(query)
+    const apiUrl2 = `https://developers.zomato.com/api/v2.1/search?city_id=297&cuisines=${query}`;
+    const headers = { 
+      'Content-Type': 'application/json',
+      'user-key': '621df21dc5fe4ac84e874b3ddaf3536e'
+    } 
+    fetch(apiUrl2, {method:'GET',headers})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            restaurants: result.restaurants
+          });
+          console.log(result)
+        },
+        (error) => {
+          console.log(error)
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   componentDidMount() {
@@ -59,7 +103,26 @@ class App extends Component {
       return (
         <div>
           <div><RestaurantDash restaurant = {restaurantInfo} /></div>
-       <div><RestaurantsList restaurants = {restaurants}  handleRestaurantClick = {this.handleRestaurantClick.bind(this)} /></div>
+          <div><RestaurantsList restaurants = {restaurants}  handleRestaurantClick = {this.handleRestaurantClick.bind(this)} /></div>
+          <div>
+            <form>
+              <input type="checkbox" id="25" name="cuisines" value="Chinese" onChange={this.handleInputChange} />
+              <label>Chinese</label><br/>
+              <input type="checkbox" id="161" name="cuisines" value="Coffee and Tea" onChange={this.handleInputChange} />
+              <label>Coffee and Tea</label><br/>
+              <input type="checkbox" id="1" name="cuisines" value="American" onChange={this.handleInputChange} />
+              <label>American</label><br/>
+              <input type="checkbox" id="4" name="cuisines" value="Arabian" onChange={this.handleInputChange} />
+              <label>Arabian</label><br/>
+              <input type="checkbox" id="1039" name="cuisines" value="Cafe Food" onChange={this.handleInputChange} />
+              <label>Cafe Food</label><br/>
+              <input type="checkbox" id="40" name="cuisines" value="Fast Food" onChange={this.handleInputChange} />
+              <label>Fast Food</label><br/>
+              <input type="checkbox" id='3' name='cuisines' value='Asian'  onChange={this.handleInputChange}/>
+              <label> Asian</label><br/><br/>
+              <button onClick={(e) => this.filterRestaurantsByCuisines(e)}>filter</button>
+            </form>
+          </div>
         </div>
       );
     }
